ea288b21e2f3007bc32a2d7c0c4f3895614e42ac
Kavanapriya <kavanapriya7@gmail.com>
2025-04-22 16:25:35 +0530

    project initiated

---
+++ src/main/java/com/example/movieticketbookingsystem/exceptions/handler/UserExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.movieticketbookingsystem.exceptions.handler;
+
+
+
+import com.example.movieticketbookingsystem.exceptions.UserExistByEmailException;
+import com.example.movieticketbookingsystem.util.ErrorStructure;
+import com.example.movieticketbookingsystem.util.RestResponseBuilder;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@AllArgsConstructor
+public class UserExceptionHandler {
+    private final RestResponseBuilder responseBuilder;
+    @ExceptionHandler
+    public ResponseEntity<ErrorStructure> handleUseExistByEmailException(UserExistByEmailException ex){
+        return  responseBuilder.errror(HttpStatus.OK, ex.getMessage());
+    }
+
+}
